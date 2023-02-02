96647571bb8a55875f7fb30d6ec3c61b0cf0f28f
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-02-02 16:36:38 +0200

    add inheritance with function constructor

--- Day 6/solution/part 2/script.js
+++ Day 6/solution/part 2/script.js
@@ -1,32 +1,68 @@
-class Shape {
-  name;
-  sides;
-  sideLength;
+// using classes
+// class Shape {
+//   name;
+//   sides;
+//   sideLength;
 
-  constructor(name,sides,sideLength){
-    this.name = name;
-    this.sides = sides;
-    this.sideLength = sideLength;
-  }
+//   constructor(name,sides,sideLength){
+//     this.name = name;
+//     this.sides = sides;
+//     this.sideLength = sideLength;
+//   }
 
-  calcPerimeter(){
-    let perimeter = this.sides * this.sideLength;
-    return perimeter;
-  }
+//   calcPerimeter(){
+//     let perimeter = this.sides * this.sideLength;
+//     return perimeter;
+//   }
+// }
+
+// class Square extends Shape{
+//   constructor(sideLength){
+//     super('square',4,sideLength);
+
+//   }
+
+//   calcArea(){
+//     let perimeter = 4 * this.sideLength;
+//     return perimeter;
+//   }
+
+// }
+
+// let square = new Square(5);
+
+// console.log(square.calcArea());
+
+
+// let triangle = new Shape('square',3,3);
+
+// console.log(triangle.calcPerimeter());
+
+
+// with function constructor
+
+function Shape (name, sides, sideLength) {
+  this.name = name;
+  this.sides = sides;
+  this.sideLength = sideLength;
 }
 
-class Square extends Shape{
-  constructor(sideLength){
-    super('square',4,sideLength);
+Shape.prototype.calcPerimeter = function (){
+  let perimeter = this.sides * this.sideLength;
+  return perimeter;
+}
 
-  }
+function Square(sideLength){
 
-  calcArea(){
-    let perimeter = 4 * this.sideLength;
-    return perimeter;
-  }
+  Shape.call(this,"square", 4, sideLength);
 
 }
+
+Square.prototype.calcArea =   function (){
+  let perimeter = 4 * this.sideLength;
+  return perimeter;
+}
+
 
 let square = new Square(5);
 
@@ -36,4 +72,3 @@ console.log(square.calcArea());
 let triangle = new Shape('square',3,3);
 
 console.log(triangle.calcPerimeter());
-
