7b0d9def1a9db08f16554b9fa7d80040e8955017
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-02-06 13:54:42 +0200

    lab 7

--- Day 6/solution/part 1/script.js
+++ Day 6/solution/part 1/script.js
@@ -1,26 +1,47 @@
-class Shape {
-  name;
-  sides;
-  sideLength;
+// with classses
+// class Shape {
+//   name;
+//   sides;
+//   sideLength;
 
-  constructor(name,sides,sideLength){
+//   constructor(name,sides,sideLength){
+//     this.name = name;
+//     this.sides = sides;
+//     this.sideLength = sideLength;
+//   }
+
+//   calcPerimeter(){
+//       let perimeter = this.sides * this.sideLength;
+//       return perimeter;
+//   }
+// }
+
+// let square = new Shape('square',4,5);
+
+// console.log(square.calcPerimeter());
+
+
+// let triangle = new Shape('triangle',3,3);
+
+// console.log(triangle.calcPerimeter());
+
+function Shape(name,sides,sideLength) {
     this.name = name;
     this.sides = sides;
     this.sideLength = sideLength;
-  }
-
-  calcPerimeter(){
-      let perimeter = this.sides * this.sideLength;
-      return perimeter;
-  }
 }
+
+Shape.prototype.calcPerimeter = function (){
+  let perimeter = this.sides * this.sideLength;
+  return perimeter;
+}
+
 
 let square = new Shape('square',4,5);
 
 console.log(square.calcPerimeter());
 
 
-let triangle = new Shape('square',3,3);
+let triangle = new Shape('triangle',3,3);
 
 console.log(triangle.calcPerimeter());
-
---
+++ Day 7/task 1/script.js
@@ -0,0 +1,30 @@
+
+function login(elem){
+
+  elem.preventDefault;
+  let myModal = new bootstrap.Modal(document.getElementById("myModal"), {
+    keyboard: false,
+  });
+
+
+
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let modalBody = document.getElementById("modal-body");
+
+  if(email === "" || password === "")
+  {
+    modalBody.innerHTML = "Please enter email and password"
+    myModal.show();
+
+  }else if(email === 'admin' && password === '123')
+  {
+    modalBody.innerHTML = "welcome"
+    myModal.show();
+  }else{
+    modalBody.innerHTML = "Sorry, you are not registered"
+    myModal.show();
+  }
+
+  
+}
